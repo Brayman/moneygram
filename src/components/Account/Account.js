f5441aee7b2a58c35fbe76ed5876b1cd9f0bba68
Artem <Ar.rakitskiy@gmail.com>
2022-04-05 19:04:41 +0400

    Создал элемент баланса

---
+++ src/components/Account/Account.js
@@ -0,0 +1,19 @@
+import style from './Account.module.css';
+function Account() {
+    console.log(style);
+    return (
+        <section className={style.account}>
+            <div className={style.balance_card}>
+                account balance:
+                <div className={style.balance}>$336 </div>
+            </div>
+        
+        <div className={style.status}>
+            spent today $15
+        </div>
+        <div className={`${style.active} ${style.empty}`}></div>
+      </section>
+    )
+    
+}
+export default Account;
