0d75b77b9579f36f7e5d520102f2665bf0745dfd
Artem <Ar.rakitskiy@gmail.com>
2022-04-06 12:54:40 +0400

    Добавил роутинг поправил стили

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import logo from './logo.png';
 import './App.css';
 import Header from './components/Header/Header';
 import Transaction from './components/Transaction/Transaction';
+import Settings from './components/Profile/Settings';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header/>
-      <Account/>
+        <Routes>
+          <Route path='/' element={<Account/>}/>
+          <Route path='/profile' element={<Settings/>}/>
+        </Routes>
+      
       <section className='transactions'>
         <Transaction cost={10} tag={"shop"}/>
         <Transaction cost={5} tag={"bus"}/>
       </section>
-    </div>
+      <Menu/>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import { BiBell } from "react-icons/bi";
 import ava from '../../images/ava.jpg'
 import style from "./Header.module.css";
 function Header(params) {
+    const navigate = useNavigate();
     return (
-        <header className={style.header}>
+        <header className={style.header} onClick={() => navigate("/profile")}>
             <img className={style.avatar} src={ava} alt='avatar'/>
             my monegram
             <BiBell/>
