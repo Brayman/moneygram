96121bcbaa1614811f0864d302957a83b87cefd6
Artem <Ar.rakitskiy@gmail.com>
2022-04-07 20:19:50 +0400

    Изменение работы и отрисовки тэгов

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,7 +1,58 @@
+import { useEffect, useState } from "react";
 import { BiCartAlt } from "react-icons/bi";
-function Tag() {
-    return (<div className="tag">
-        <BiCartAlt/>
-    </div>)
+import { 
+    RiTaxiLine,
+    RiTakeawayLine,
+    RiRestaurant2Fill,
+    RiPlaneFill,
+    RiStethoscopeLine,
+    RiGiftLine,
+    RiWifiLine,
+    RiScissorsFill,
+    RiMoneyDollarCircleLine,
+    RiForbidLine
+ } from "react-icons/ri";
+function Tag({tag}) {
+    const [icon, setIcon] = useState(<RiForbidLine/>)
+    
+    useEffect(() =>{
+        switch (tag) {
+        case 'taxi':
+            setIcon(<RiTaxiLine/>)
+            break;
+        case 'deliver':
+            setIcon(<RiTakeawayLine/>)
+            break;
+        case 'shop':
+            setIcon(<BiCartAlt/>)
+            break;
+        case 'health':
+            setIcon(<RiStethoscopeLine/>)
+            break;
+        case 'beauty':
+            setIcon(<RiScissorsFill/>)
+            break;
+        case 'ethernet':
+            setIcon(<RiWifiLine/>)
+            break;
+        case 'travel':
+            setIcon(<RiPlaneFill/>)
+            break;
+        case 'gift':
+            setIcon(<RiGiftLine/>)
+            break;
+
+    
+        default:
+
+            break;
+    }
+    },[tag])
+    
+    return (
+    <div className="tag">
+        {icon}
+    </div>
+    )
 }
 export default Tag;
--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -1,11 +1,11 @@
 import style from "./Transaction.module.css"
 import Tag from "../Tag";
-function Transaction({cost, tag}) {
+function Transaction({body}) {
     return (
         <section className={style.transaction}>
-          <Tag tag={tag}/>
-            <div>{tag}</div>
-            - ${cost}
+          <Tag tag={body.tag}/>
+            <div>{body.tag}</div>
+            - ${body.cost}
         </section>
     )
 }
