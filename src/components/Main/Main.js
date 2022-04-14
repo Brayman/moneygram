d3cf94c13b068783369bbc3437b85a64b022db0c
Artem <Ar.rakitskiy@gmail.com>
2022-04-14 14:31:20 +0400

    настройка стилей

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,7 +3,7 @@ import Transaction from "../Transaction/Transaction";
 
 function Main({accountData, transactions}) {
         return (
-        <div>
+        <div className="home">
             <Account data={accountData} />
             <section className='transactions'>
                 {transactions.map((item, i) => <Transaction key={i} body={item}/>)}
