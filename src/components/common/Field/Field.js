35ca8f19fbf8046b59e413f253425a0a9086c572
Artem <Ar.rakitskiy@gmail.com>
2022-06-08 14:43:35 +0400

    Вынес отдельно поля ввода

---
+++ src/components/common/Field/Field.js
@@ -0,0 +1,21 @@
+import { useField, useFormikContext } from 'formik'
+import React from 'react';
+import "./style.css";
+
+export const Field = (props) => {
+    const [field, meta] = useField(props)
+    const error = !!meta.error && "field__error field_error" 
+    return (
+        <>
+            <input
+                {...field}
+                {...props}
+                className={`${props.className} field ${error}`}
+            />
+            {!!meta.touched && !!meta.error && 
+            <div className="field__error">
+                {meta.error}
+            </div>}
+        </>
+    )
+}
