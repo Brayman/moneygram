3d7a370272b8cdde5437b7ab5329bd949f98f1b4
Artem <Ar.rakitskiy@gmail.com>
2022-06-09 22:16:01 +0400

    Перенёс верстку страницы транзакций, и переделал иконку тэга

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,24 +1,67 @@
+import { useNavigate } from "react-router-dom";
 import Account from "../Account/Account";
 import Loader from "../common/Loader/Loader";
-import Transaction from "../Transaction/Transaction";
+import TransactionMini from "../Transaction/TransactionMini";
+import style from './MainPage.module.css';
+import {
+    MdFilterList,
+    MdKeyboardArrowDown,
+    MdKeyboardArrowRight,
+    MdShoppingCart
+} from "react-icons/md";
 
-function Main({props}) {
+function Main({ props }) {
     if (!props.isLoading) {
-        return (<div className="home">
-            <Account data={props.account} />
+        return (
+            <section className="transactions">
+                <header className="transactions__header">
+                    <button className="filter-btn">
+                        <MdKeyboardArrowDown className="filter-btn__icon" />
+                        Month
+                    </button>
+                    <button className="icon-btn">
+                        <MdFilterList />
+                    </button>
+                </header>
+                <button className="info-btn transactions__report">
+                    See your financial report
+                    <MdKeyboardArrowRight className="info-btn__icon" />
+                </button>
+                <section className="day-section transactions__item">
+                    <header className="day-section__header">
+                        Today
+                    </header>
+                    {props.transactions.map((item, i) => {
+                        return (<TransactionMini
+                            key={item.id}
+                            onClick={() => {
+                                props.openTransaction(item);
+                            }}
+                            body={item}
+                        />)
+                    })}
+                </section>
+
+
+            </section>
+        )
+        return (<div className={style.main}>
             <section className='transactions'>
-                {props.transactions.map((item, i) => <Transaction key={i} body={item}/>)}
+
                 {
                     props.maxPage !== props.curentPage ?
-                    <button disabled={props.moreTransLoad} onClick={()=>props.getNextPage(props.curentPage + 1)}>more</button> :
-                    null
+                        <button
+                            disabled={props.moreTransLoad}
+                            onClick={() => props.getNextPage(props.page + 1)}
+                        >
+                            more
+                        </button> :
+                        null
                 }
-                
             </section>
         </div>)
     } else {
-        return <Loader/>
+        return <Loader />
     }
-    
 }
 export default Main;
