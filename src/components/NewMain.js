056e995aeef64c6bf8a7b5bfb92de436f5aa9038
Artem <Ar.rakitskiy@gmail.com>
2022-06-09 16:35:24 +0400

    Наверстал страницу с транзакциями

--- src/components/NewMain.js
+++ src/components/NewMain.js
@@ -2,167 +2,70 @@ import "./NewMain.css";
 import {
     MdFilterList,
     MdKeyboardArrowDown,
+    MdKeyboardArrowRight,
     MdShoppingCart
 } from "react-icons/md";
 
+
+import React from 'react'
+
+export const Transaction = () => {
+    return (
+        <section className="transaction">
+            <MdShoppingCart className="transaction__icon" />
+            <div className="transaction__content">
+                <div className="content__row transaction__title">
+                    <div>
+                        Shopping
+                    </div>
+                    <div className="cost cost_expense">
+                        -120 $
+                    </div>
+                </div>
+                <div className="content__row transaction__subtitle">
+                    <div>
+                        Something buying
+                    </div>
+                    <div>
+                        07:30 PM
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
 function Main(params) {
     return (
-        <section>
-            <header className="main__header">
-                <button className="button button__select">
-                    <MdKeyboardArrowDown className="button-select__icon" />
-                    <span>Month</span>
+        <section className="transactions">
+            <header className="transactions__header">
+                <button className="filter-btn">
+                    <MdKeyboardArrowDown className="filter-btn__icon" />
+                    Month
                 </button>
-                <button className="icon-button">
+                <button className="icon-btn">
                     <MdFilterList />
                 </button>
             </header>
-            <main className="main__list">
-                <h4 className="transaction__title">
+            <button className="info-btn transactions__report">
+                See your financial report
+                <MdKeyboardArrowRight className="info-btn__icon" />
+            </button>
+            <section className="day-section transactions__item">
+                <header className="day-section__header">
                     Today
-                </h4>
-                <section className="transaction__item">
-                    <div className="transaction__icon">
-                        <MdShoppingCart />
-                    </div>
-                    <div className="transaction__content">
-                        <div className="content__title">
-                            <div className="content__name">
-                                Food
-                            </div>
-                            <div className="content__money_expense">
-                                16 GEL
-                            </div>
-                        </div>
-                        <div className="content__subtitle">
-                            <div className="content__payee">
-                                Yammy
-                            </div>
-                            <div className="content__date">
-                                03/06/22
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section className="transaction__item">
-                    <div className="transaction__icon">
-                        <MdShoppingCart />
-                    </div>
-                    <div className="transaction__content">
-                        <div className="content__title">
-                            <div className="content__name">
-                                Food
-                            </div>
-                            <div className="content__money">
-                                16 GEL
-                            </div>
-                        </div>
-                        <div className="content__subtitle">
-                            <div className="content__payee">
-                                Yammy
-                            </div>
-                            <div className="content__date">
-                                03/06/22
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section className="transaction__item">
-                    <div className="transaction__icon">
-                        <MdShoppingCart />
-                    </div>
-                    <div className="transaction__content">
-                        <div className="content__title">
-                            <div className="content__name">
-                                Food
-                            </div>
-                            <div className="content__money">
-                                16 GEL
-                            </div>
-                        </div>
-                        <div className="content__subtitle">
-                            <div className="content__payee">
-                                Yammy
-                            </div>
-                            <div className="content__date">
-                                03/06/22
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section className="transaction__item">
-                    <div className="transaction__icon">
-                        <MdShoppingCart />
-                    </div>
-                    <div className="transaction__content">
-                        <div className="content__title">
-                            <div className="content__name">
-                                Food
-                            </div>
-                            <div className="content__money">
-                                16 GEL
-                            </div>
-                        </div>
-                        <div className="content__subtitle">
-                            <div className="content__payee">
-                                Yammy
-                            </div>
-                            <div className="content__date">
-                                03/06/22
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <h4 className="transaction__title">
+                </header>
+                <Transaction />
+                <Transaction />
+                <Transaction />
+            </section>
+            <section className="day-section transactions__item">
+                <header className="day-section__header">
                     Yesterday
-                </h4>
-                <section className="transaction__item">
-                    <div className="transaction__icon">
-                        <MdShoppingCart />
-                    </div>
-                    <div className="transaction__content">
-                        <div className="content__title">
-                            <div className="content__name">
-                                Food
-                            </div>
-                            <div className="content__money">
-                                16 GEL
-                            </div>
-                        </div>
-                        <div className="content__subtitle">
-                            <div className="content__payee">
-                                Yammy
-                            </div>
-                            <div className="content__date">
-                                03/06/22
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section className="transaction__item">
-                    <div className="transaction__icon">
-                        <MdShoppingCart />
-                    </div>
-                    <div className="transaction__content">
-                        <div className="content__title">
-                            <div className="content__name">
-                                Food
-                            </div>
-                            <div className="content__money">
-                                16 GEL
-                            </div>
-                        </div>
-                        <div className="content__subtitle">
-                            <div className="content__payee">
-                                Yammy
-                            </div>
-                            <div className="content__date">
-                                03/06/22
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </main>
+                </header>
+                <Transaction />
+                <Transaction />
+            </section>
         </section>
     )
 }
