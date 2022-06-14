f1efc56ae4d47aa98cf8ecb9d2757c142167cbfe
Artem <Ar.rakitskiy@gmail.com>
2022-06-14 21:20:25 +0400

    Наверстал страницу выбора кошелька

---
+++ src/components/Accounts/Accounts.js
@@ -0,0 +1,52 @@
+import { useSelector } from "react-redux"
+import { compose } from "redux";
+import './Accounts.css';
+import { WithAuthRedirect } from "../../hoc/withAuthRedirect";
+import * as selectors from "../../redux/selectors"
+import { NavLink } from "react-router-dom";
+import { BiWallet } from "react-icons/bi";
+import { Navigation } from "../common/Navigation/Navigation";
+
+function Accounts() {
+    const cards = useSelector(selectors.cards);
+    return (
+
+        <CardList cards={cards} />
+
+    )
+}
+function CardList({ cards }) {
+    return (
+        <section  className="accounts">
+            <Navigation title="Accounts" className="accounts__nav" />
+            <section className="accounts__balance">
+                <div className="balance__subtitle">
+                    Account balance
+                </div>
+                <div className="balance__title">
+                    USD 1905
+                </div>
+            </section>
+            <menu className="accounts__list">
+                {cards.map(card => {
+                    return <NavLink
+                        className="wallet"
+                        to={`/transactions/${card.id}`}
+                        key={card.id}>
+                        <BiWallet className="wallet__icon" />
+                        <div className="wallet__name">
+                            {card.name}
+                        </div>
+                        <div className="wallet__balance">
+                            {`${card.currency} ${card.balance}`}
+                        </div>
+                    </NavLink>
+                })}
+            </menu>
+            <NavLink className="accounts__button primary-btn" to={'/create-card'}>
+                Add new card
+            </NavLink>
+        </section>
+    )
+}
+export default compose(WithAuthRedirect)(Accounts);
