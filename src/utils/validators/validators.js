8b2318ad6469bc4627a00c2f8ccbe1ea1500e512
Artem <Ar.rakitskiy@gmail.com>
2022-06-15 16:56:51 +0400

    Написал страницу входа с верификацией полей и ошибок запросов

--- src/utils/validators/validators.js
+++ src/utils/validators/validators.js
@@ -1,7 +1,23 @@
+import * as Yup from "yup";
+
+const email =  Yup.string()
+.email('Invalid email address')
+.required('This required field');
+
+export const validateYup = Yup.object({
+    login: Yup.string()
+        .min(6, "Must be more 6 characters")
+        .required('This required field'),
+    password: Yup.string()
+    .min(6, "Must be more 6 characters")
+    .required('This required field'),
+})
+console.log(validateYup);
 export const validate = values => {
+
     const errors = {};
-    if (!values.email) {
-        errors.email = 'This field not to be empty'
+    if (!values.login) {
+        errors.login = 'This field not to be empty'
     }
     if (!values.password) {
         errors.password = 'This field not to be empty'
