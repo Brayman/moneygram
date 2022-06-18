7430cbf0c9a598ee7798e7ce76cd960dc3a85ac0
Artem <Ar.rakitskiy@gmail.com>
2022-06-18 23:31:28 +0400

    Сделал страницу профиля

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,48 +1,79 @@
 import Account from "../Account/Account";
-import { BiChevronRight, BiChevronLeft } from "react-icons/bi";
+import {
+    BiChevronRight,
+    BiChevronLeft,
+    BiPlus,
+    BiUser
+} from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import Cards from "../Cards/CardsComponent";
 import { WithAuthRedirect } from "../../hoc/withAuthRedirect";
-import { connect } from "react-redux";
+import style from "./Settings.module.css";
+import { useEffect } from "react";
+import { accountThunks } from "../../redux/actions/account-actions";
+import { compose } from "redux";
+import { connect, useSelector } from "react-redux";
+import { setPreviousCardAC, setNextCardAC } from "../../redux/account";
+import * as selectors from "../../redux/selectors"
+import "./Profile.css"
 
-function ArrowButton({props, setNextCard, setPervCard}) {
-    switch (props) {
-        case 'left':
-            return (
-                <button className="left_button" onClick={() => setPervCard()}>
-                    <BiChevronLeft size={30}/>
-                </button>
-            )
-        case 'right':
-            return (
-                <button className="right_button"  onClick={() => setNextCard()}>
-                    <BiChevronRight size={30}/>
-                </button>
-            )
-        default:
-            return <noscript/>
+function Profile({ cards, userid, account, loadUser, setPervCard, setNextCard, selectCard }) {
+    useEffect(() => {
+        loadUser(userid)
+    }, [])
+    const navigate = useNavigate();
+    const user = useSelector(selectors.account)
+    return (
+        <section className="profile">
+            <header className="profile__header">
+                <div className="avatar">
+                    {user.avatar == null ? <BiUser className="avatar__image" /> : <img className="avatar__image" src={user.avatar} />}
+                </div>
+                <div className="profile__title">
+                    <span className="profile-header__subtitle">
+                        Name
+                    </span>
+                    <span className="profile-header__title">
+                        {`${user.first_name} ${user.second_name}`}
+                    </span>
+                </div>
+            </header>
+            <menu className="profile__menu">
+                <div className="menu__option">
+                    <span>
+                        Account
+                    </span>
+                </div>
+                <div className="menu__option">
+                    <span>
+                        Settings
+                    </span>
+                </div>
+                <div className="menu__option">
+                    <span>
+                        Logout
+                    </span>
+                </div>
+            </menu>
+        </section>
+    )
+}
+const mapStateToProps = state => {
+    return {
+        userid: state.account.id,
+        cards: state.card.cards,
+        selectCard: state.card.selectCard
     }
 }
 
-function Profile({account,setUser, card, setCards, setNextCard, setPervCard}) {
-    const navigate = useNavigate();
-   
-    return (
-        <div  className="home">
-            <section className='cards'>
-               <ArrowButton props='left' setPervCard={setPervCard}/>
-               <Cards card={card} setCards={setCards} setUser={setUser}/>
-               <ArrowButton props='right' setNextCard={setNextCard}/>
-            </section>
-            
-            <p>{account.login}</p>
-            <p>{account.first_name}</p>
-            <p>{account.second_name}</p>
-            <p>{account.mail}</p>
-            <button onClick={() => navigate("/settings")}>
-                Settings
-            </button>
-        </div>
-    )
+const mapDispatchToProps = dispatch => {
+    return {
+        loadUser: (id) => dispatch(accountThunks.loadUser(id)),
+        setPervCard: () => dispatch(setPreviousCardAC()),
+        setNextCard: () => dispatch(setNextCardAC()),
+    }
 }
-export default WithAuthRedirect(Profile);
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    WithAuthRedirect
+)(Profile)
