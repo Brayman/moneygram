9795e542c47210fb936113d26e0d9e302d35aa32
Artem <Ar.rakitskiy@gmail.com>
2022-06-23 14:33:56 +0400

    Rewrite Button, create IconButton

---
+++ src/components/common/Button/Buttons.js
@@ -0,0 +1,41 @@
+import './Buttons.css';
+import classNames from 'classnames'
+import {cn} from '@bem-react/classname'
+import React from 'react';
+
+const createClassName = (primary, secondary) => {
+    const buttonType = classNames({
+        'primary-button': primary,
+        'secondary-button': secondary,
+        'ghost-button': !primary && !secondary,
+    })
+    return cn(buttonType);
+}
+
+
+
+export const Button = ({active, primary, secondary, className, children, onClick }) => {
+    
+    
+    const bcn = createClassName(primary, secondary)
+    return (
+        <button
+            className={classNames(bcn({active}), className)}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    )
+}
+
+export const IconButton = ({className, onClick, ...props}) => {
+    const bcn = createClassName()
+    return (
+        <button
+            className={classNames('icon-button',bcn(), className)}
+            onClick={onClick}
+        >
+            <props.icon className="icon-button__icon"/>
+        </button>
+    )
+}
