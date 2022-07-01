df43eee484f4958c797b32dec37c4e4a6c65fa7c
Artem <Ar.rakitskiy@gmail.com>
2022-07-01 20:38:56 +0400

    add Pie rendering and data adapter function

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -10,31 +10,81 @@ class ChartPage extends Component {
 
         this.state = {
             options: {
-                chart: {
-                    id: "basic-bar"
+                line: {
+                    chart: {
+                        type: "line",
+                        id: "basic-bar",
+                        width: "95%"
+                    }
+                },
+                bar: {
+                    plotOptions: {
+                        bar: {
+                            horizontal: true
+                        }
+                    },
+                    chart: {
+                        type: "pie",
+                        id: "donut",
+                        width: "50%"
+                    },
+                    labels: this.getPieData(this.props.data, 'tag')
                 }
             },
-            series: [{
-                data: this.props.data.map(transaction => {
-                    return {x: new Date(transaction.date).getTime(), y: transaction.cost}
-                })
-            }],
+            series: {
+                line: [{
+                    data: this.props.data.map(transaction => {
+                        return { x: new Date(transaction.date).getTime(), y: transaction.cost }
+                    })
+                }],
+                bar: this.getPieData(this.props.data, 'amount')
+
+            },
             xaxis: {
                 type: 'datetime'
             }
         };
     }
+    getPieData(transactions, field) {
+        
+        let tags = [];
+        let amounts = [];
+        transactions.forEach(({ tag, cost }) => {
+            debugger
+            const i = tags.findIndex((item) => item === tag)
+            if (i !== -1) {
+                amounts[i] = cost + amounts[i]
 
+            } else {
+                tags.push(tag)
+                amounts.push(cost)
+            }
+        })
+        switch (field) {
+            case 'tag':
+                return tags
+            case 'amount':
+                return amounts
+            default:
+                break;
+        }
+    }
     render() {
         return (
             <div className="app">
                 <div className="row">
                     <div className="mixed-chart">
                         <Chart
-                            options={this.state.options}
-                            series={this.state.series}
-                            type="line"
-                            width="95%"
+                            options={this.state.options.line}
+                            series={this.state.series.line}
+                            type={this.state.options.line.chart.type}
+                            width='90%'
+                        />
+                        <Chart
+                            options={this.state.options.bar}
+                            series={this.state.series.bar}
+                            type={this.state.options.bar.chart.type}
+                            width='50%'
                         />
                     </div>
                 </div>
