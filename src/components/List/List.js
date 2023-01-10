31869a8472e5b0ab3963d0289fafd1c91098b108
Artem <Ar.rakitskiy@gmail.com>
2023-01-10 17:25:58 +0300

    Added division of transactions by days

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,16 +2,57 @@ import classNames from 'classnames'
 import React from 'react'
 import TransactionMini from '../Transaction/TransactionPreview'
 import Loader from '../common/Loader/Loader'
+import styled from 'styled-components'
 
-export const List = ({isLoading, transactions, className}) => {
+const TitleStyled = styled.div`
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 21.8px;
+`
+
+const DateTitle = ({ children }) => {
+    return <TitleStyled>
+        {children}
+    </TitleStyled>
+}
+
+export const List = ({ isLoading, transactions, className }) => {
+    function selectTitle() {
+        let localDate = new Date(0)
+        return (dayDate) => {
+            if (localDate.getDate() !== new Date(dayDate).getDate()) {
+                localDate = new Date(dayDate)
+                const dayAgo = new Date().getDate() - localDate.getDate()
+                switch (dayAgo) {
+                    case 0:
+                        return <DateTitle key={localDate.getTime()}>
+                            Today
+                        </DateTitle>
+                    case 1:
+                        return <DateTitle key={localDate.getTime()}>
+                            Yerstaday
+                        </DateTitle>
+                    default:
+                        return <DateTitle key={localDate.getTime()}>
+                            {new Date(localDate).toLocaleDateString()}
+                        </DateTitle>
+                }
+            }
+            else return false
+        }
+
+    }
+    const getTitle = selectTitle()
     return (
         <section className={classNames("day-section transactions__item", className)}>
-            {!isLoading ? transactions.map((item, i) => {
-                return (<TransactionMini
-                    key={item._id}
-                    
-                    body={item}
-                />)
+            {!isLoading ? transactions.map((item) => {
+                return (<>
+                    {getTitle(new Date(item.date))}
+                    <TransactionMini
+                        key={item._id}
+                        body={item}
+                    />
+                </>)
             }) : <Loader />}
         </section>
     )
